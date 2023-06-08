36b62e4120cca19735ca75212d1ccfbadd0364ec
Rohan0004 <rohanraut0004@gmail.com>
2023-06-08 13:42:08 +0530

    Encapsulation Basics

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+        //name is private, When we directly access private member of class by reference variable it gives error.
+        //obj.name="Accio";
+        //System.out.println(obj.name);
+        //java: name has private access in com.driver.RWOnly
+
+        //To access private member we use getter(to get value) and setter(to set value);
+        obj.setName("Accio");
+        System.out.println(obj.getName());
+    }
 }
